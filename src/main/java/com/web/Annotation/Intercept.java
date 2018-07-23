afa419192a59ab3d1daabcb7db75f3bae5196ba0
Jiangyiran0308 <35513643+Jiangyiran0308@users.noreply.github.com>
2018-07-23 17:37:09 +0800

    Library

---
+++ src/main/java/com/web/Annotation/Intercept.java
@@ -0,0 +1,19 @@
+package com.web.Annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author: Jiang
+ * @Date: Created in 17:19  2018\7\23 0023
+ * @Description:
+ * @Modified By:
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface Intercept {
+    String desc() ;
+    String author();
+    int age() default 18 ;
+}
